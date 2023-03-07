66f84b1ede0db39e798b68f29aae3fd605c7d413
tmayweather <tnea.mayweather@gmail.com>
2023-03-07 08:11:51 -0500

    refactoring

--- main.py
+++ main.py
@@ -1,56 +1,24 @@
-import calendar
-import datetime
 import pandas as pd
+import re
+import PyPDF2
 import SendMessage as sm
 
 # sm.send_message()
 
-j = pd.read_csv('jointjanfeb23.csv')
-m = pd.read_csv('mainjanfeb23.csv')
-c_payment = []
-income = []
-sum_income = 0
-sum_c_payment = 0
+joint = pd.read_csv('jointjanfeb23.csv')
+main = pd.read_csv('mainjanfeb23.csv')
+df = pd.concat([joint, main], axis=0)
+df.to_csv('fin.csv', index=False)
+print(df)
 
+start_date = '2023-01-01'
+end_date = '2023-01-31'
+date_mask = (df['Date'] >= start_date) & (df['Date'] <= end_date)
+df_range = df.loc[date_mask]
 
-cc_payments_df = j[j['Category'] == 'Credit Card Payment']
-income_df = m[m['Category'] == 'Paycheck']
+amounts_per_category = df_range.groupby('Category')['Amount'].sum()
+print(amounts_per_category)
 
-
-
-# Extract the values in the 'Amount' and 'Description' columns and append them to the c_payment list
-def get_desc_amt():
-    for index, row in cc_payments_df.iterrows():
-        c_payment.append((row['Description'], row['Amount']))
-
-    for index, row in income_df.iterrows():
-        income.append((row['Description'], row['Amount']))
-
-
-get_desc_amt()
-
-for payment in c_payment:
-    for e in payment:
-        print(e)
-
-for credit_cards in c_payment:
-    c_name = credit_cards[0]
-    c_amount = abs(credit_cards[1])
-    print(f'The monthly payment made on {c_name} is ${c_amount}')
-
-for paychecks in income:
-    p_name = paychecks[0]
-    p_amount = paychecks[1]
-    sum_income += paychecks[1]
-
-
-print(f'Our monthly income is ${sum_income}')
-
-
-for payments in c_payment:
-    sum_c_payment += abs(payments[1])
-
-print(f'The total amount paid on Credit Cards is ${sum_c_payment}')
 
 
 

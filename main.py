e551aaa90ee871c9bba45e1637ce1b80cc73e128
tmayweather <tnea.mayweather@gmail.com>
2023-03-09 11:36:42 -0500

    working on scheduling

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
 import pandas as pd
-import re
-import PyPDF2
 import SendMessage as sm
 
 # sm.send_message()
 
-joint = pd.read_csv('jointjanfeb23.csv')
-main = pd.read_csv('mainjanfeb23.csv')
+joint = pd.read_csv('/home/tmayweather/finance/jointjanfeb23.csv')
+main = pd.read_csv('/home/tmayweather/finance/mainjanfeb23.csv')
 df = pd.concat([joint, main], axis=0)
 df.to_csv('fin.csv', index=False)
+read_file = pd.read_excel('cc.xlsx')
 print(df)
+read_file.to_csv('cc.csv',
+                 index=False,
+                 header=True)
+cc = pd.DataFrame(pd.read_csv('cc.csv', index_col=0))
+print(cc)
 
-start_date = '2023-01-01'
-end_date = '2023-01-31'
-date_mask = (df['Date'] >= start_date) & (df['Date'] <= end_date)
-df_range = df.loc[date_mask]
+start_date = ''
+end_date = ''
 
-amounts_per_category = df_range.groupby('Category')['Amount'].sum()
-print(amounts_per_category)
+
+def filter_date(start_date, end_date):
+    date_mask = (df['Date'] >= start_date) & (df['Date'] <= end_date)
+    df_range = df.loc[date_mask]
+    amounts_per_category = df_range.groupby('Category')['Amount'].sum()
+    print(amounts_per_category)
+
+
+filter_date('2023-01-01', '2023-01-31')
 
 
 
